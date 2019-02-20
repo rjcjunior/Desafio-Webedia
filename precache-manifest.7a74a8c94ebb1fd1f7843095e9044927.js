6ea9e5c2b859eb953ddc07dd6d5c09cd827192c3
Ricardo <rjcjunior@hotmail.com>
2019-02-20 00:38:27 -0300

    Updates

---
+++ precache-manifest.7a74a8c94ebb1fd1f7843095e9044927.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "010839011687174de98204e9686c07b2",
+    "url": "/Desafio-Webedia/static/media/errorImg.01083901.png"
+  },
+  {
+    "revision": "7f690e503a254e0b8349aec0177e07aa",
+    "url": "/Desafio-Webedia/static/media/Lato-Regular.7f690e50.ttf"
+  },
+  {
+    "revision": "7aa00e8402613d0110b9",
+    "url": "/Desafio-Webedia/static/js/runtime~main.7aa00e84.js"
+  },
+  {
+    "revision": "101cbe5c04eccd486252",
+    "url": "/Desafio-Webedia/static/js/main.101cbe5c.chunk.js"
+  },
+  {
+    "revision": "3161286f832d1ae1b6c2",
+    "url": "/Desafio-Webedia/static/js/1.3161286f.chunk.js"
+  },
+  {
+    "revision": "101cbe5c04eccd486252",
+    "url": "/Desafio-Webedia/static/css/main.7010ee22.chunk.css"
+  },
+  {
+    "revision": "0070ff7691d0e4e14060cb0592febb47",
+    "url": "/Desafio-Webedia/index.html"
+  }
+];
